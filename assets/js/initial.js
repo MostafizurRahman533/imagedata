e43531d46ac3dd755b1a2ac941671686d1d64248
md fahad <mdfahad01618@gmail.com>
2022-07-23 23:28:58 +0600

    today

--- assets/js/initial.js
+++ assets/js/initial.js
@@ -78,3 +78,33 @@ breakpoints: {
   },
 },
 });
+var swiper = new Swiper(".trust-prof-slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+});
+var swiper = new Swiper(".service-slider", {
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".modal-swiper", {
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
